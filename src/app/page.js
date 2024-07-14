ae21fb5e0ca46ebcc2b6305f7e4991578589d9af
Rodrigo Cousillas <cousillas.rodrigo@gmail.com>
2024-07-14 20:27:48 -0300

    Llamada a API

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,38 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+
+import { useEffect, useState } from 'react';
+//import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const [phrase, setPhrase] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPhrase = async () => {
+      try {
+        const res = await fetch('https://frasedeldia.azurewebsites.net/api/phrase');
+        if (!res.ok) {
+          throw new Error('Error al obtener la frase');
+        }
+        const data = await res.json();
+        setPhrase(data);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchPhrase();
+  }, []);
+
   return (
-    
-    <>
-      <h1>Coming soon!</h1>
-    </>
+    <div>
+      <h1>Frase del Día</h1>
+      {error ? (
+        <p>Error: {error}</p>
+      ) : (
+        <p>{phrase ? phrase.text : 'Cargando...'}</p>
+      )}
+    </div>
   );
 }
+
